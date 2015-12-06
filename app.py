e30748e4041c7e92014fcb645393d3ca72bd5db4
egonline <elango.online@outlook.com>
2015-12-06 14:05:01 +0900

    v2..

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     message = ("<h1>Welcome HemphiKid Friends!</h1><br />"
-               "We are building our own Minecraft server!<br />"
+               "We are building our own Minecraft server v2!<br />"
                "Keep watching here because it is coming soon!<br />")
     return message
 
